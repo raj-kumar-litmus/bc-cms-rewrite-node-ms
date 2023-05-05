3346e62286de2b7a7bd8edd484243a1383f7707e
Raj Kumar <coding.godzilla@gmail.com>
2023-05-05 09:03:13 +0530

    Initial express commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3010;
+
+app.get('/mock', (req, res) => {
+  console.log('Calling mock api');
+  res.sendStatus(200)
+})
+
+app.listen(PORT);
+console.log(`Running Backend on PORT : ${PORT}`);
