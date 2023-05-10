5817cdf95752cd585025203034b6d8282f99f64c
Raj Kumar <coding.godzilla@gmail.com>
2023-05-10 15:13:46 +0530

    Feat : Login AD integration

---
+++ src/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  development: {
+    hostnameClient: 'http://localhost:3000',
+    hostnameServer: 'http://localhost:3010',
+    clientSecret: 'QwP8Q~pMWdlDO_2QYtz0.lYbOAc1yfPue9LD1dal',
+    defaultMsGraphScope : 'https://graph.microsoft.com/.default',
+    msGraphHostName: 'https://graph.microsoft.com/v1.0',
+    msLoginHostName: 'https://login.microsoftonline.com',
+    tenantId: '420b9f05-caaa-4e33-9309-3eeb115514fd',
+    clientId:'f7fdabfa-274a-4401-852d-b448a62f70d6',
+  },
+  staging : {
+
+  },
+  production : {
+
+  }
+}
