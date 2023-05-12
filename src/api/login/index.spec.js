25127aee9008127d5861e7fda29c48bb5c564aa7
Raj Kumar <coding.godzilla@gmail.com>
2023-05-12 17:18:49 +0530

    Rewrite app methods

---
+++ src/api/login/index.spec.js
@@ -0,0 +1,9 @@
+
+// todo.
+// 1. finish validate jwt api;
+// 2. write test cases;
+// 3. define folder structure.
+// 4. migrate fetch member groups to backend.
+// 5. build get members of groups in backend.
+// 6. fix me token issue.
+// https://dreamix.eu/blog/frontpage/node-js-project-structure-a-short-guide
