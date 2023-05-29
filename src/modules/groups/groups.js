2fe0c5e6d4528f706130a659cd897fc0fe7e0afc
nikhilchandrabackcountry <132487558+nikhilchandrabackcountry@users.noreply.github.com>
2023-05-29 17:33:16 +0530

    [fix] Group members api

--- src/modules/groups/groups.js
+++ src/modules/groups/groups.js
@@ -1,55 +1,68 @@
 const express = require('express');
 const axios = require('axios');
 const { validateMiddleware } = require('../../middlewares');
-const { groupDto } = require('./dtos');
+const { groupsDto } = require('./dtos');
 
 const router = express.Router();
 
+const {
+  CLIENT_SECRET,
+  CLIENT_ID,
+  DEFAULT_MS_GRAPH_SCOPE,
+  MS_LOGIN_HOST_NAME,
+  TENANT_ID,
+  MS_GRAPH_HOST_NAME,
+  WRITERS_GROUP_ID,
+  EDITOR_GROUP_ID
+} = process.env;
+
 const getAccessToken = async () => {
-  const { CLIENT_SECRET, CLIENT_ID, DEFAULT_MS_GRAPH_SCOPE, MS_LOGIN_HOST_NAME, TENANT_ID } =
-    process.env;
-  try {
-    const URL = `${MS_LOGIN_HOST_NAME}/${TENANT_ID}/oauth2/v2.0/token`;
-    const { data } = await axios.post(
-      URL,
-      {
-        grant_type: 'client_credentials',
-        client_secret: CLIENT_SECRET,
-        client_id: CLIENT_ID,
-        scope: DEFAULT_MS_GRAPH_SCOPE
-      },
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data'
-        }
+  const URL = `${MS_LOGIN_HOST_NAME}/${TENANT_ID}/oauth2/v2.0/token`;
+  const {
+    data: { access_token: accessToken }
+  } = await axios.post(
+    URL,
+    {
+      grant_type: 'client_credentials',
+      client_secret: CLIENT_SECRET,
+      client_id: CLIENT_ID,
+      scope: DEFAULT_MS_GRAPH_SCOPE
+    },
+    {
+      headers: {
+        'Content-Type': 'multipart/form-data'
       }
-    );
-    return data?.access_token ? `Bearer ${data?.access_token}` : undefined;
-  } catch (error) {
-    return null;
-  }
+    }
+  );
+  return accessToken ? `Bearer ${accessToken}` : undefined;
 };
 
 router.get('/token', async (req, res) => {
-  const token = await getAccessToken();
-  if (!token) {
-    return res.sendResponse('UnAuthorized', 401);
+  try {
+    const accessToken = await getAccessToken();
+
+    return res.sendResponse(
+      {
+        accessToken
+      },
+      200
+    );
+  } catch (error) {
+    return res.sendResponse('Error while fetching token', 401);
   }
-  return res.sendResponse(
-    {
-      accessToken: token
-    },
-    200
-  );
 });
 
-router.get('/all/:type/members', validateMiddleware({ params: groupDto }), async (req, res) => {
+router.get('/:type/members', validateMiddleware({ params: groupsDto }), async (req, res) => {
   const { type } = req.params;
-  const { MS_GRAPH_HOST_NAME, WRITERS_GROUP_ID, EDITOR_GROUP_ID } = process.env;
-  const accessToken = await getAccessToken();
-  if (!accessToken) {
-    return res.sendResponse('UnAuthorized', 401);
+
+  let accessToken;
+  try {
+    accessToken = await getAccessToken();
+  } catch (error) {
+    console.error(error);
+    return res.sendResponse('Failed to fetch access token', 500);
   }
+
   let groupId;
   /* eslint-disable indent */
   switch (type) {
@@ -62,25 +75,21 @@ router.get('/all/:type/members', validateMiddleware({ params: groupDto }), async
     default:
       break;
   }
-  /* eslint-enable indent */
 
   try {
     const URL = `${MS_GRAPH_HOST_NAME}/groups/${groupId}/members`;
     const {
-      data: { value }
+      data: { value: members }
     } = await axios.get(URL, {
       headers: {
         Authorization: accessToken
       }
     });
-    return res.sendResponse(
-      {
-        members: value
-      },
-      200
-    );
+
+    return res.sendResponse(members, 200);
   } catch (error) {
-    return res.sendResponse(error, 400);
+    console.error(error);
+    return res.sendResponse('Failed to fetch members', 500);
   }
 });
 
