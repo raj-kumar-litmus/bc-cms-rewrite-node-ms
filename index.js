5ef0bbb527eec3eee657f9290a9719159d06d009
RajKumar-BackCountry <130644214+RajKumar-BackCountry@users.noreply.github.com>
2023-07-17 19:28:43 +0530

    log pubsub errs

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const app = require('./src/app');
 const { listenForMessages } = require('./src/pubsub');
 const { properties } = require('./src/properties');
+const { logger } = require('./src/lib/logger');
 
 const { NETSUITE_PUBSUB_TOPIC } = properties;
 
@@ -9,5 +10,9 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
   console.log(properties);
-  listenForMessages(NETSUITE_PUBSUB_TOPIC);
+  try {
+    listenForMessages(NETSUITE_PUBSUB_TOPIC);
+  } catch (err) {
+    logger.error({ err }, 'Error occuring while listening to pubsub calls');
+  }
 });
