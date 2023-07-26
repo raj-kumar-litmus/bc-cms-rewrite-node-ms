b1b782ffe1c3b90ecf89222142b587a407f289bb
nikhilchandrabackcountry <132487558+nikhilchandrabackcountry@users.noreply.github.com>
2023-07-26 19:17:49 +0530

    [feat] All POST Apis of Sizing app

--- src/modules/sizing/dtos.js
+++ src/modules/sizing/dtos.js
@@ -1,10 +1,34 @@
 const Joi = require('joi');
 
+const sizeSchema = Joi.object({
+  id: Joi.number().integer(),
+  description: Joi.string().required(),
+  name: Joi.string().required(),
+  position: Joi.number().integer()
+});
+
+const scaleSchema = Joi.object({
+  name: Joi.string().required(),
+  description: Joi.string().required(),
+  sizes: Joi.array().items(sizeSchema)
+});
+
 const createSizeValueDto = Joi.object({
   name: Joi.string().required(),
   description: Joi.string()
 });
 
+const sizeMappingSchema = Joi.object({
+  sizeId: Joi.number().required(),
+  preferredSizeId: Joi.number().required()
+});
+
+const arrayOfSizeMappingsSchema = Joi.array().items(sizeMappingSchema);
+
 module.exports = {
-  createSizeValueDto
+  arrayOfSizeMappingsSchema,
+  sizeSchema,
+  scaleSchema,
+  createSizeValueDto,
+  sizeMappingSchema
 };
--- src/modules/sizing/sizing.js
+++ src/modules/sizing/sizing.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const axios = require('axios');
+const Joi = require('joi');
 
 const router = express.Router();
 
 const { postgresPrisma } = require('../prisma');
 const { logger } = require('../../lib/logger');
-const { validateMiddleware } = require('../../middlewares');
-const { createSizeValueDto } = require('./dtos');
+const { authorize, validateMiddleware } = require('../../middlewares');
+const { groups } = require('../../properties');
+const { scaleSchema, createSizeValueDto, arrayOfSizeMappingsSchema } = require('./dtos');
 
 router.get('/scales/all', async (req, res) => {
   try {
@@ -14,8 +16,8 @@ router.get('/scales/all', async (req, res) => {
     return res.sendResponse({
       scales
     });
-  } catch (err) {
-    logger.error({ err }, 'Error occured while fetching scales');
+  } catch (error) {
+    logger.error({ error }, 'Error occured while fetching scales');
     return res.sendResponse('Internal Server Error', 500);
   }
 });
@@ -29,8 +31,8 @@ router.get('/scales/details/:scaleid', async (req, res) => {
     return res.sendResponse({
       scales
     });
-  } catch (err) {
-    logger.error({ err }, 'Error occured while fetching scale details');
+  } catch (error) {
+    logger.error({ error }, 'Error occured while fetching scale details');
     return res.sendResponse('Internal Server Error', 500);
   }
 });
@@ -41,8 +43,8 @@ router.get('/sizevalues', async (req, res) => {
     return res.sendResponse({
       scales
     });
-  } catch (err) {
-    logger.error({ err }, 'Error occured while fetching size values');
+  } catch (error) {
+    logger.error({ error }, 'Error occured while fetching size values');
     return res.sendResponse('Internal Server Error', 500);
   }
 });
@@ -55,23 +57,282 @@ router.get('/productgroups', async (req, res) => {
     return res.sendResponse({
       productgroups
     });
-  } catch (err) {
-    logger.error({ err }, 'Error occured while fetching productgroups');
+  } catch (error) {
+    logger.error({ error }, 'Error occured while fetching productgroups');
     return res.sendResponse('Internal Server Error', 500);
   }
 });
 
+const createScale = async (req, res) => {
+  try {
+    const { name, description, sizes } = req.body;
+
+    const createdScale = await postgresPrisma.dn_scales.create({
+      data: {
+        name,
+        description,
+        dn_sizes: {
+          create: sizes
+        }
+      },
+      include: {
+        dn_sizes: true
+      }
+    });
+
+    res.sendResponse(createdScale, 201);
+  } catch (error) {
+    console.error(error);
+    res.sendResponse('Error occurred while creating the scale', 500);
+  }
+};
+
+router.post(
+  '/scales',
+  authorize([
+    groups.ADMIN_GROUP_NAME,
+    groups.WRITER_GROUP_NAME,
+    groups.EDITOR_GROUP_NAME,
+    groups.SIZING_GROUP_NAME
+  ]),
+  validateMiddleware({
+    body: scaleSchema
+  }),
+  createScale
+);
+
+const updateScale = async (req, res) => {
+  try {
+    const { scaleId } = req.params;
+    const { name, description, sizes } = req.body;
+
+    const updatedScale = await postgresPrisma.dn_scales.update({
+      where: { id: parseInt(scaleId) },
+      data: {
+        name,
+        description,
+        dn_sizes: {
+          upsert: sizes.map((size) => ({
+            where: { id: size.id ? parseInt(size.id) : -1 },
+            create: size,
+            update: size
+          }))
+        }
+      },
+      include: {
+        dn_sizes: true
+      }
+    });
+
+    return res.sendResponse(updatedScale, 200);
+  } catch (error) {
+    if (error.code === 'P2025') {
+      logger.error(
+        { error },
+        'Error occurred while updating the scale: dn_scales record not found'
+      );
+      return res.sendResponse('Scale not found', 404);
+    }
+    logger.error({ error }, 'Error occurred while updating the scale');
+    return res.sendResponse('Error occurred while updating the scale', 500);
+  }
+};
+
+router.put(
+  '/scales/:scaleId',
+  authorize([
+    groups.ADMIN_GROUP_NAME,
+    groups.WRITER_GROUP_NAME,
+    groups.EDITOR_GROUP_NAME,
+    groups.SIZING_GROUP_NAME
+  ]),
+  validateMiddleware({
+    body: scaleSchema
+  }),
+  updateScale
+);
+
+const setPreferredScaleForProductGroup = async (req, res) => {
+  try {
+    const { productgroupId, scaleId } = req.params;
+
+    const mostRecentPreferredScale = await postgresPrisma.dn_preferredscales.findFirst({
+      where: {
+        productgroup: parseInt(productgroupId)
+      },
+      orderBy: {
+        last_modified: 'desc'
+      },
+      take: 1
+    });
+
+    const upsertResult = await postgresPrisma.dn_preferredscales.upsert({
+      where: {
+        id: mostRecentPreferredScale?.id || undefined
+      },
+      update: {
+        scaleid: parseInt(scaleId)
+      },
+      create: {
+        productgroup: parseInt(productgroupId),
+        scaleid: parseInt(scaleId),
+        brand: 0
+      }
+    });
+
+    return res.sendResponse(upsertResult, 201);
+  } catch (error) {
+    logger.error({ error }, 'Error occurred while updating the preferred Scale');
+    return res.sendResponse('Error occurred while updating the preferred Scale', 500);
+  }
+};
+
+router.put(
+  '/productgroups/:productgroupId/preferredScale/:scaleId',
+  authorize([
+    groups.ADMIN_GROUP_NAME,
+    groups.WRITER_GROUP_NAME,
+    groups.EDITOR_GROUP_NAME,
+    groups.SIZING_GROUP_NAME
+  ]),
+  setPreferredScaleForProductGroup
+);
+
 router.post(
   '/sizevalue/add',
+  authorize([
+    groups.ADMIN_GROUP_NAME,
+    groups.WRITER_GROUP_NAME,
+    groups.EDITOR_GROUP_NAME,
+    groups.SIZING_GROUP_NAME
+  ]),
   validateMiddleware({ body: createSizeValueDto }),
   async (req, res) => {
     try {
       const { name, description } = req.body;
       await postgresPrisma.$queryRaw`INSERT INTO dn_sizevalues(name,description) VALUES (${name},${description});`;
       return res.sendResponse({});
-    } catch (err) {
-      logger.error({ err }, 'Error occured while inserting size values');
+    } catch (error) {
+      logger.error({ error }, 'Error occured while inserting size values');
       return res.sendResponse('Internal Server Error', 500);
+    }
+  }
+);
+
+const getSizeMappings = async (scaleId, preferredScaleId) => {
+  try {
+    const result = await postgresPrisma.$queryRaw`
+      SELECT smp.* FROM dn_sizemappings smp 
+      JOIN dn_sizes sz ON smp.size = sz.id 
+      JOIN dn_scales sc ON sz.scaleid = sc.id
+      JOIN dn_sizes psz ON smp.preferredsize = psz.id
+      JOIN dn_scales psc ON psz.scaleid = psc.id
+      WHERE sc.id = ${parseInt(scaleId)} AND psc.id = ${parseInt(preferredScaleId)}
+      order by psz.position, sz.position;
+    `;
+    return result;
+  } catch (error) {
+    logger.error({ error }, 'Error occurred while fetching size mappings');
+    throw error;
+  }
+};
+
+const insertSizeMappings = async (sizeMappingsData) => {
+  try {
+    const createdMappings = await Promise.all(
+      sizeMappingsData.map(async (mapping) => {
+        const createdMapping = await postgresPrisma.dn_sizemappings.create({
+          data: {
+            dn_sizes_dn_sizemappings_sizeTodn_sizes: {
+              connect: { id: parseInt(mapping.sizeId) }
+            },
+            dn_sizes_dn_sizemappings_preferredsizeTodn_sizes: {
+              connect: { id: parseInt(mapping.preferredSizeId) }
+            }
+          }
+        });
+
+        return createdMapping;
+      })
+    );
+
+    logger.info({ createdMappings }, 'Successfully inserted size mappings');
+  } catch (error) {
+    logger.error({ error }, 'Error occurred while inserting size mappings');
+  } finally {
+    await postgresPrisma.$disconnect();
+  }
+};
+
+const deleteSizeMappings = async (mappingIds) => {
+  try {
+    await postgresPrisma.dn_sizemappings.deleteMany({
+      where: {
+        id: {
+          in: mappingIds
+        }
+      }
+    });
+
+    logger.info({ mappingIds }, 'Successfully deleted size mappings');
+  } catch (error) {
+    logger.error({ error }, 'Error occurred while deleting size mappings');
+  } finally {
+    await postgresPrisma.$disconnect();
+  }
+};
+
+router.post(
+  '/sizeMapping/:preferredScaleId/:scaleId',
+  authorize([
+    groups.ADMIN_GROUP_NAME,
+    groups.WRITER_GROUP_NAME,
+    groups.EDITOR_GROUP_NAME,
+    groups.SIZING_GROUP_NAME
+  ]),
+  validateMiddleware({ body: arrayOfSizeMappingsSchema }),
+  async (req, res) => {
+    try {
+      const { preferredScaleId, scaleId } = req.params;
+      const sizeMappings = req.body;
+
+      const existingSizeMappings = await getSizeMappings(scaleId, preferredScaleId);
+
+      await deleteSizeMappings(existingSizeMappings.map(({ id }) => id));
+
+      await insertSizeMappings(sizeMappings);
+
+      return res.sendResponse();
+    } catch (error) {
+      logger.error({ error }, 'Error occurred while creating size mappings');
+      return res.sendResponse('Error occurred while creating size mappings', 500);
+    }
+  }
+);
+
+router.delete(
+  '/sizeMapping/:preferredScaleId',
+  authorize([
+    groups.ADMIN_GROUP_NAME,
+    groups.WRITER_GROUP_NAME,
+    groups.EDITOR_GROUP_NAME,
+    groups.SIZING_GROUP_NAME
+  ]),
+  validateMiddleware({ body: Joi.array().items(Joi.number().integer()) }),
+  async (req, res) => {
+    try {
+      const { preferredScaleId } = req.params;
+      const scales = req.body;
+
+      for (const scaleId of scales) {
+        const existingSizeMappings = await getSizeMappings(scaleId, preferredScaleId);
+        await deleteSizeMappings(existingSizeMappings.map(({ id }) => id));
+      }
+
+      return res.sendResponse('Size mappings deleted successfully');
+    } catch (error) {
+      logger.error({ error }, 'Error occurred while deleting size mappings:');
+      return res.sendResponse('Error occurred while deleting size mappings', 500);
     }
   }
 );
