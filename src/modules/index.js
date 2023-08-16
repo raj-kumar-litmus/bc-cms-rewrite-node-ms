54dafc3c6ab45be4a70bc0b04fcf54d6a5b3289f
RajKumar-BackCountry <130644214+RajKumar-BackCountry@users.noreply.github.com>
2023-08-16 13:06:53 +0530

    [fix] Update index.js

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,11 +3,15 @@ const express = require('express');
 const { workflows } = require('./workflows');
 const { dataNormalization } = require('./dataNormalization');
 const { groups } = require('./groups');
+const { styles } = require('./styles');
+const { sizing } = require('./sizing');
 
 const router = express.Router();
 
 router.use('/workflows', workflows);
 router.use('/dataNormalization', dataNormalization);
+router.use('/styles', styles);
 router.use('/groups', groups);
+router.use('/sizing', sizing);
 
 module.exports = router;
